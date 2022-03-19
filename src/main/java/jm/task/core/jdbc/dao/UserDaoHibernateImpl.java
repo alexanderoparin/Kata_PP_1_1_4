aad18671977d982ad172733b7562f82394773c55
Alexander Oparin <alexanderoparin@mail.ru>
2022-03-19 22:32:46 +0300

    не проходят тест getAllUsers

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
@@ -10,13 +11,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
     }
 
     @Override
     public void createUsersTable() {
         Transaction transaction = null;
-        try (final Session session = Util.getSessionFactory().openSession()) {
+        try (final Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, " +
                     "name VARCHAR(45), lastName VARCHAR(45), age TINYINT(100));";
@@ -33,7 +37,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         Transaction transaction = null;
-        try (final Session session = Util.getSessionFactory().openSession()) {
+        try (final Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "DROP TABLE IF EXISTS users;";
             session.createSQLQuery(sql).addEntity(User.class);
@@ -48,10 +52,11 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
         Transaction transaction = null;
-        try (final Session session = Util.getSessionFactory().openSession()) {
+        try (final Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.save(new User(name, lastName, age));
+            session.persist(user);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -65,7 +70,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (final Session session = Util.getSessionFactory().openSession()) {
+        try (final Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.delete(session.get(User.class, id));
             transaction.commit();
@@ -80,18 +85,26 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        List<User> allUser = new ArrayList<>();
-        try (final Session session = Util.getSessionFactory().openSession()) {
-            allUser = (List<User>) session.createQuery("FROM User").list();
-            System.out.println("Лист из Users создан");
+        List<User> userList = new ArrayList<>();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("SELECT u FROM User u", User.class).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
-        return allUser;
+        System.out.println("Лист из Users создан");
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
         Transaction transaction = null;
-        try (final Session session = Util.getSessionFactory().openSession()) {
+        try (final Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             String sql = "TRUNCATE TABLE users;";
             Query query = session.createSQLQuery(sql);
